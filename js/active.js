3aac6ce5002ce0ce971edf4763b4b17c9e8b72a4
Abir Bouhriz Daidj <bouhrizabir2015@gmail.com>
2020-08-10 18:25:07 +0100

    menu section

---
+++ js/active.js
@@ -0,0 +1,14 @@
+(function ($) {
+"use strict";
+
+/*--------------------------------------
+	mixitup Active
+----------------------------------------*/
+var mixer = mixitup('.portfolio-item',{
+	animation:{
+		effectsOut:'fade translateX(-100%)',
+	}
+});
+
+
+})(jQuery);	
--- script.js
+++ script.js
@@ -10,3 +10,5 @@ signUpButton.addEventListener('click', () => {
 signInButton.addEventListener('click', () => {
 	container.classList.remove("right-panel-active");
 });
+
+//menu section
