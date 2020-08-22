aa66a823f02988ced0d0f5a4f8837b8ee1756a0d
Abir Bouhriz Daidj <bouhrizabir2015@gmail.com>
2020-08-22 21:40:57 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,83 @@
+(function($) {
+	"use strict"
+
+	// Preloader
+	//$(window).on('load', function() {
+	//	$("#preloader").delay(600).fadeOut();
+	//});
+	
+	// Mobile Toggle Btn
+	$('.navbar-toggle').on('click',function(){
+		$('#header').toggleClass('nav-collapse');
+	});
+	
+	// Fixed Nav
+	$(window).on('scroll', function() {
+		var wScroll = $(this).scrollTop();
+		wScroll > $('.banner-area').height() ? $('#header').addClass('fixed') : $('#header').removeClass('fixed');
+	});
+	
+	// Banner Area Height
+	function bannerHeight () {
+		$('.banner-area').css({'paddingTop': $('#header').height() + 30});
+	}
+	$(window).on('resize', function() {
+		bannerHeight();
+	});
+	bannerHeight();
+	
+	// Gallery Slider
+	$('#Gallery').owlCarousel({
+		items:2,
+		loop:true,
+		margin:0,
+		dots : false,
+		nav: true,
+		navText : ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
+		autoplay : true,
+		responsive:{
+			0: {
+				items:1
+			},
+			992:{
+				items:2
+			}
+		}
+	});
+
+})(jQuery);
+
+//sign form
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+
+signUpButton.addEventListener('click', () => {
+	container.classList.add("right-panel-active");
+});
+
+signInButton.addEventListener('click', () => {
+	container.classList.remove("right-panel-active");
+});
+
+
+//menu section
+const inputs = document.querySelectorAll(".input");
+
+function focusFunc() {
+  let parent = this.parentNode;
+  parent.classList.add("focus");
+}
+
+function blurFunc() {
+  let parent = this.parentNode;
+  if (this.value == "") {
+    parent.classList.remove("focus");
+  }
+}
+
+inputs.forEach((input) => {
+  input.addEventListener("focus", focusFunc);
+  input.addEventListener("blur", blurFunc);
+});
+
